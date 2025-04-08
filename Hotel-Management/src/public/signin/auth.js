72c81393b5894b75b15f4b53e012c0158baeedc1
Dhiraj <141422204+Imdhiraj007@users.noreply.github.com>
2025-04-08 23:57:34 +0530

    added some files

---
+++ Hotel-Management/src/public/signin/auth.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('form');
+  
+    form.addEventListener('submit', async (e) => {
+      e.preventDefault();
+  
+      var username = form.username.value;
+      var password = form.password.value;
+      const credentials = {
+        email: username,
+        password: password
+    };
+ 
+      console.log("Sending login request with:", { username, password, role });
+
+      if (!username || !password || !role) {
+        alert("Please fill in all fields.");
+        return;
+      }
+  
+      try {
+     
+        
+        
+       
+        
+        
+        
+       
+        
+        const serverUrl = 'http://localhost:3000/login';
+        
+        axios.post(serverUrl, credentials)
+        .then(response => {
+              window.location.href = '/dashboard'
+        })
+         .catch(error => {
+            console.log(error);
+            })
+      
+      } catch (error) {
+        console.error('Login error:', error);
+        alert('Server error. Please try again later.');
+      }
+    });
+  });
+  
---
+++ Hotel-Management/src/service/mongo.js
@@ -0,0 +1,20 @@
+const mongoose= require('mongoose');//mongodb 
+const MONGO_URL='';
+// adding event listeners
+mongoose.connection.once('open',()=>{
+    console.log('mongodb connection ready');
+});
+mongoose.connection.on('error',(err)=>{ 
+    console.error(err);
+});
+const options = {
+    useNewUrlParser: true,
+  };
+
+async function mongoConnect() {
+    mongoose.connect(MONGO_URL,options);
+}
+
+module.exports = {
+    mongoConnect,
+};
